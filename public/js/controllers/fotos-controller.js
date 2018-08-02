c1266d77c6b603502dcc6118ba69c5a90a354128
Felipe Lima <fe.max@hotmail.com>
2018-08-02 08:44:39 -0300

    validando formulario

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,6 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
 	$scope.fotos = [];
-	$scope.foto = {};
 	$scope.filtro = '';
 
 	$http.get('/v1/fotos')
@@ -12,9 +11,5 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	.error(function(erro) {
 		console.log(erro);
 	});
-
-	$scope.submeter = () => {
-		console.log($scope.foto);
-	}
 
 });
